bd34a98edf4925502e8bbdf4503537158fb3ef0f
DedoDeda <91195108+DedoDeda@users.noreply.github.com>
2023-01-25 16:14:06 +0200

    Documented the code.

--- app/src/main/java/com/example/xo/MainActivity.java
+++ app/src/main/java/com/example/xo/MainActivity.java
@@ -10,15 +10,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    /** The text that displays all the messages to the players. */
     private TextView messageTextView;
+    /** The button to restart the game. */
     private Button restartButton;
-    private final Cell[][] cells;
+    /** A cell matrix representing the board. */
+    private final Cell[][] board;
+    /** Is it X's turn now? */
     private boolean xTurn;
+    /** The number of used cells. */
     private int numFilledCells;
+    /** Are we waiting for restart (after win, draw...). */
     private boolean waitingForRestart;
 
+    /** Constructs an empty default board. */
     public MainActivity() {
-        cells = new Cell[3][3];
+        board = new Cell[3][3];
         xTurn = true;
     }
 
@@ -34,32 +41,36 @@ public class MainActivity extends AppCompatActivity {
 
     private void initRestartButton() {
         restartButton = findViewById(R.id.btn_restart);
-
+        // Bind to the restart button click event.
         restartButton.setOnClickListener(v -> restartGame());
     }
 
     private void initCells() {
-        cells[0][0] = new Cell(findViewById(R.id.imgview_cell_1));
-        cells[0][1] = new Cell(findViewById(R.id.imgview_cell_2));
-        cells[0][2] = new Cell(findViewById(R.id.imgview_cell_3));
-        cells[1][0] = new Cell(findViewById(R.id.imgview_cell_4));
-        cells[1][1] = new Cell(findViewById(R.id.imgview_cell_5));
-        cells[1][2] = new Cell(findViewById(R.id.imgview_cell_6));
-        cells[2][0] = new Cell(findViewById(R.id.imgview_cell_7));
-        cells[2][1] = new Cell(findViewById(R.id.imgview_cell_8));
-        cells[2][2] = new Cell(findViewById(R.id.imgview_cell_9));
+        // Initialize the board with the views.
+        board[0][0] = new Cell(findViewById(R.id.imgview_cell_1));
+        board[0][1] = new Cell(findViewById(R.id.imgview_cell_2));
+        board[0][2] = new Cell(findViewById(R.id.imgview_cell_3));
+        board[1][0] = new Cell(findViewById(R.id.imgview_cell_4));
+        board[1][1] = new Cell(findViewById(R.id.imgview_cell_5));
+        board[1][2] = new Cell(findViewById(R.id.imgview_cell_6));
+        board[2][0] = new Cell(findViewById(R.id.imgview_cell_7));
+        board[2][1] = new Cell(findViewById(R.id.imgview_cell_8));
+        board[2][2] = new Cell(findViewById(R.id.imgview_cell_9));
 
-        for (int rowIdx = 0; rowIdx < cells.length; rowIdx++) {
-            Cell[] row = cells[rowIdx];
+        // Bind to each cell's click event.
+        for (int rowIdx = 0; rowIdx < board.length; rowIdx++) {
+            Cell[] row = board[rowIdx];
             final int finalY = rowIdx;
             for (int columnIdx = 0; columnIdx < row.length; columnIdx++) {
                 Cell cell = row[columnIdx];
                 final int finalX = columnIdx;
                 cell.image.setOnClickListener(v -> {
+                    // Do nothing if we're waiting for restart or if this cell is already taken.
                     if (waitingForRestart || cell.state != CellState.Empty) {
                         return;
                     }
 
+                    // Mark the cell as taken and update the turn state message accordingly.
                     if (xTurn) {
                         cell.setX();
                         messageTextView.setText(getResources().getText(R.string.o_turn));
@@ -72,8 +83,9 @@ public class MainActivity extends AppCompatActivity {
 
                     if (hasWinner(finalY, finalX)) {
                         endGameWithWinner();
-                    } else if (numFilledCells == 9) {
-                        endGameWithoutWinner();
+                    } else if (isBoardFull()) {
+                        // If the board is full and we don't have a winner yet it's a draw.
+                        endGameWithDraw();
                     }
 
                     xTurn = !xTurn;
@@ -82,30 +94,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void endGameWithoutWinner() {
+    /** Are all the cells in the board taken? */
+    private boolean isBoardFull() {
+        return numFilledCells == 9;
+    }
+
+    /** Ends the game displaying a draw message. */
+    private void endGameWithDraw() {
+        // Lock the board until a restart.
         waitingForRestart = true;
 
+        // Display the message.
         CharSequence drawText = getResources().getText(R.string.draw);
         messageTextView.setText(drawText);
         Toast.makeText(this, drawText, Toast.LENGTH_SHORT).show();
     }
 
+    /** Ends the game declaring the winner. */
     private void endGameWithWinner() {
+        // Declare the winner.
         CharSequence winText;
         if (xTurn) {
             winText = getResources().getText(R.string.x_won);
         } else {
             winText = getResources().getText(R.string.o_won);
         }
-
         messageTextView.setText(winText);
         Toast.makeText(this, winText, Toast.LENGTH_SHORT).show();
 
+        // Lock the board until a restart.
         waitingForRestart = true;
     }
 
+    /** Clears all the game's data. */
     private void restartGame() {
-        for (Cell[] row : cells) {
+        for (Cell[] row : board) {
             for (Cell cell : row) {
                 cell.clear();
             }
@@ -118,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /** Is there a winner (AKA a row, diagonal or column X or Y sequence)? */
     private boolean hasWinner(int changedRow, int changedColumn) {
         if (hasRowWin(changedRow)) {
             return true;
@@ -130,53 +154,64 @@ public class MainActivity extends AppCompatActivity {
         return hasDiagonalWin();
     }
 
+    /** Is there a diagonal X or O sequence. */
     private boolean hasDiagonalWin() {
-        if (cells[0][0].state == cells[1][1].state
-                && cells[1][1].state == cells[2][2].state
-                && cells[2][2].state != CellState.Empty) {
+        if (board[0][0].state == board[1][1].state
+                && board[1][1].state == board[2][2].state
+                && board[2][2].state != CellState.Empty) {
             return true;
         }
 
-        return cells[0][2].state == cells[1][1].state
-                && cells[1][1].state == cells[2][0].state
-                && cells[2][0].state != CellState.Empty;
+        return board[0][2].state == board[1][1].state
+                && board[1][1].state == board[2][0].state
+                && board[2][0].state != CellState.Empty;
     }
 
+    /** Is there a column X or O sequence. */
     private boolean hasColumnWin(int column) {
-        return cells[0][column].state == cells[1][column].state
-                && cells[1][column].state == cells[2][column].state
-                && cells[2][column].state != CellState.Empty;
+        return board[0][column].state == board[1][column].state
+                && board[1][column].state == board[2][column].state
+                && board[2][column].state != CellState.Empty;
     }
 
+    /** Is there a row X or O sequence. */
     private boolean hasRowWin(int row) {
-        return cells[row][0].state == cells[row][1].state
-                && cells[row][1].state == cells[row][2].state
-                && cells[row][2].state != CellState.Empty;
+        return board[row][0].state == board[row][1].state
+                && board[row][1].state == board[row][2].state
+                && board[row][2].state != CellState.Empty;
     }
 
+    /** A board's cell state. */
     private enum CellState {
         Empty, X, O,
     }
 
+    /** A cell in the game's board. */
     private static class Cell {
+        /** The cell's image. */
         public ImageView image;
+        /** The cell's state. */
         public CellState state;
 
+        /** Construct an empty cell given it's ImageView in the display. */
         public Cell(ImageView image) {
             this.image = image;
             this.state = CellState.Empty;
         }
 
+        /** Marks the cell as X. */
         public void setX() {
             image.setImageResource(R.drawable.cell_x);
             state = CellState.X;
         }
 
+        /** Marks the cell as Y. */
         public void setO() {
             image.setImageResource(R.drawable.cell_o);
             state = CellState.O;
         }
 
+        /** Clears the cell's data. */
         public void clear() {
             image.setImageResource(R.drawable.cell_empty);
             state = CellState.Empty;
